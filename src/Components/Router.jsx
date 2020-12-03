3192d6de714e99f197897cba3d512ed7ee2c46c3
eunjin0212 <eunjin9639@gmail.com>
2020-12-03 17:43:16 +0900

    Navi Router

---
+++ src/Components/Router.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
+import Navgation from "./Navgation";
+
+export default () => (
+  <Router>
+    <>
+      <Navgation />
+    </>
+  </Router>
+);
